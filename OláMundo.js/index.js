360657fc54df8ec28d509749b4f4a6fef402ed44
jesusgm0 <gabrielmarioj1@gmail.com>
2022-09-08 09:20:28 -0300

    Arquivo Olá Mundo Gabriel Mario

---
+++ OláMundo.js/index.js
@@ -0,0 +1,2 @@
+let msg
+alert("Olá Mundo")
---
+++ index.js
@@ -0,0 +1,15 @@
+let nome, idade, filhos, sonho, filme, gostoMusical
+//alert(nome)
+nome = prompt("Escreva seu nome: ")
+idade = prompt("Escreva sua idade: ")
+filhos = prompt("Você têm filhos? ")
+sonho = prompt("Qual é o seu maior sonho? ")
+filme = prompt("Qual é o seu filme favorito? ")
+gostoMusical = ("Qual é o seu gosto musical favorito? ")
+
+alert("Nome: "+ nome +
+ "\nIdade: "+ idade +
+ "\nFilhos: " + filhos +
+ "\nSonho: "+ sonho +
+  "\nFilme favorito: "+ filme+
+   "\nGosto Musical: "+ gostoMusical)
